b66497da0c25fd1c8708dcd1d2764e4dfd55fc4b
eniyavants <eniyavants@gmail.com>
2020-02-04 22:19:03 +1300

    Update test.java

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//This is test program
 
 public class test    {
     private static WebDriver driver;
